139cbd81b01c48210656aefdabb282ac07de8dd0
samanthac02 <72635022+samanthac02@users.noreply.github.com>
2022-03-07 14:47:08 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Nerd Herd Programming'
+copyright = '2021, Samantha Chang'
+author = 'Samantha Chang'
 
 release = '0.1'
 version = '0.1.0'
