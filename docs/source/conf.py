3f9bea409b30e1629b51b27a6d3a376684dc9b04
samanthac02 <72635022+samanthac02@users.noreply.github.com>
2022-04-14 23:22:33 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Nerd Herd Programming'
-copyright = '2021, Samantha Chang'
-author = 'Samantha Chang'
+copyright = '2022, Nerd Herd'
+author = 'Nerd Herd'
 
 release = '0.1'
 version = '0.1.0'
